3ec300b09b9d461d5c5e7403fe09bea75516a859
klyuvitkin Artur <kluvitkin@phoenix-cg.ru>
2022-02-23 16:20:12 +0500

    fix random function

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const getRandomInt = function (min, max) {
 
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 getRandomInt(0, 10);
