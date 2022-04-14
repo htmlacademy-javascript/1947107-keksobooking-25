e103ae95bf58d1c0017c5dbab7a423ddc83b68e9
klyuvitkin Artur <kluvitkin@phoenix-cg.ru>
2022-04-14 13:37:45 +0500

    fix

--- js/map.js
+++ js/map.js
@@ -21,12 +21,12 @@ const tileLayer = L.tileLayer(
   },
 );
 const mainMarkerIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
+  iconUrl: './img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
 const markerIcon = L.icon({
-  iconUrl: '../img/pin.svg',
+  iconUrl: './img/pin.svg',
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
